1f7bab9814456e467d81a0ce992b6eb09a3de1f9
Five <five@railway.app>
2023-05-29 17:52:19 +0800

    main: app

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name: "run",
+				Action: func(cCtx *cli.Context) error {
+					imageRef := cCtx.Args().First()
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
