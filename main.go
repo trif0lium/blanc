1f9609de93e151def4c4dfa610a8d09c28a62714
Five <five@railway.app>
2023-05-29 17:54:10 +0800

    main: /var/lib/blanc

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"os"
 
+	gonanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/urfave/cli/v2"
+)
+
+var (
+	WORKING_DIRECTORY = "/var/lib/blanc"
 )
 
 func main() {
@@ -13,6 +18,10 @@ func main() {
 				Name: "run",
 				Action: func(cCtx *cli.Context) error {
 					imageRef := cCtx.Args().First()
+					vmID, err := gonanoid.New("abcdefghijklmnopqrstuvwxyz", 17)
+					if err != nil {
+						return err
+					}
 					return nil
 				},
 			},
