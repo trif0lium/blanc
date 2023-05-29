80f11e781a33cef9ae3747a606ca34e285797e6b
Five <five@railway.app>
2023-05-29 18:14:11 +0800

    main: skopeo + umoci

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	gonanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/urfave/cli/v2"
@@ -21,6 +23,40 @@ func main() {
 
 					vmID, err := gonanoid.Generate("abcdefghijklmnopqrstuvwxyz", 17)
 					if err != nil {
+						return err
+					}
+
+					workingDir := filepath.Join(WORKING_DIRECTORY, "vms/vm_"+vmID)
+
+					if err := os.MkdirAll(workingDir, 0755); err != nil {
+						return err
+					}
+
+					if err := os.MkdirAll(filepath.Join(workingDir, "container/unpacked", 0755)); err != nil {
+						return err
+					}
+
+					cmd := exec.CommandContext(
+						cCtx.Context,
+						"skopeo",
+						"copy",
+						"docker://"+imageRef,
+						"oci:"+filepath.Join(workingDir, "container")+":latest",
+					)
+
+					if err := cmd.Run(); err != nil {
+						return err
+					}
+
+					cmd := exec.CommandContext(
+						cCtx.Context,
+						"umoci",
+						"unpack",
+						"--image", filepath.Join(workingDir, "container")+":latest",
+						filepath.Join(workingDir, "container/unpacked")
+					)
+
+					if err := cmd.Run(); err != nil {
 						return err
 					}
 
